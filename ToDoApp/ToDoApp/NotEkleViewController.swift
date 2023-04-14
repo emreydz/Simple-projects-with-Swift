23c57e0b40809a4d28aad7ddfaac280d18e7e690
Emre Yıldız <ey56748@gmail.com>
2023-04-15 00:22:05 +0300

    complate app

---
+++ ToDoApp/ToDoApp/NotEkleViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NotEkleViewController.swift
+//  NotUygulamasi
+//
+//  Created by Kasım Adalan on 30.07.2019.
+//  Copyright © 2019 info. All rights reserved.
+//
+
+import UIKit
+
+class NotEkleViewController: UIViewController {
+    @IBOutlet weak var textfieldDersAdi: UITextField!
+    
+    @IBOutlet weak var textfieldNot1: UITextField!
+    @IBOutlet weak var textfieldNot2: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func notEkle(_ sender: Any) {
+        if let ad = textfieldDersAdi.text ,let not1 = textfieldNot1.text ,let not2 = textfieldNot2.text {
+            
+            if let n1 = Int(not1) , let n2 = Int(not2) {
+                
+                Notlardao().notEkle(ders_adi: ad, not1: n1, not2: n2)
+                
+            }
+        }
+    }
+}
